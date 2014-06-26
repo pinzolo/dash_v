f5b8202d4a09e85567c3aad1f2eec2e68ca8aa5a
pinzolo <pinzolo@gmail.com>
2014-06-26 10:39:53 +0900

    Apply to Vagrant

---
+++ lib/dashv/scanners/simple_with_prefix_scanner.rb
@@ -0,0 +1,12 @@
+module Dashv::Scanners
+  class SimpleWithPrefixScanner
+    PATTERN = /\A[\w\s]+(?<major>\d+)\.(?<minor>\d+)\.(?<tiny>\d+)\z/.freeze
+
+    def scan(text)
+      match_data = PATTERN.match(text)
+      { major: match_data[:major], minor: match_data[:minor], tiny: match_data[:tiny] } if match_data
+    end
+  end
+end
+
+
---
+++ spec/apps/vagrant_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'dashv'
+
+describe 'Vagrant' do
+  it 'supported' do
+    expect(Dashv.supported?(:vagrant)).to eq true
+  end
+  context 'when Vagrant version is 1.5.3' do
+    before do
+      stub_invoke('Vagrant 1.5.3')
+    end
+    it 'returns version 1.5.3' do
+      expect(Dashv[:vagrant].version).to eq '1.5.3'
+    end
+  end
+end
+
